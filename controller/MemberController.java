8e21a32977d950de8e39252dfb8a62de0e3c6aea
rmsgh2392 <rmsgh2392@daum.net>
2019-08-24 17:18:43 +0900

    190824

--- controller/MemberController.java
+++ controller/MemberController.java
@@ -1,5 +1,124 @@
 package com.bitcamp.controller;
-
-public class MemberController {
-
+import javax.swing.JOptionPane;
+import com.bitcamp.domain.MemberBean;
+import com.bitcamp.service.MemberService;
+public class MemberController{
+	public static void main(String[] args) {
+		MemberBean member = null;
+		MemberService service = new MemberService();
+		String temp = "";
+		String[] arr = null;
+	while(true) {
+		switch (JOptionPane.showInputDialog("0.종료\n"
+				+ "1.회원가입\n"
+				+ "2.마이페이지\n"
+				+ "3.비번수정\n"
+				+ "4.회원탈퇴\n"
+				+ "5.아이디 존재여부체크\n"
+				+ "6.로그인\n"
+				+ "7.회원목록\n"
+				+ "8.아이디 검색\n"
+				+ "9.이름 검색\n"
+				+ "10.전체 회원수")){
+		case "0":
+			JOptionPane.showMessageDialog(null, "시스템 종료하겠습니다.");
+			return;
+		case "1":
+//			1.회원가입 화면
+			temp = JOptionPane.showInputDialog("이름,아이디,비밀번호,"
+					+ "주민번호,혈액형,키,몸무게");
+			member = new MemberBean();
+//			Q/여기서 생성자를 만들어주는 이유가 While문에 넣어서 반복해서 회원수를 더 늘리려고 여기다 써주는건가요?
+			 arr = temp.split(",");
+			member.setName(arr[0]);
+			member.setId(arr[1]);
+			member.setPw(arr[2]);
+			member.setSsn(arr[3]);
+			member.setBlood(arr[4]);
+			member.setHeight(Double.parseDouble(arr[5]));
+			member.setWeight(Double.parseDouble(arr[6]));
+			JOptionPane.showMessageDialog(null, service.join(member));
+			break;
+		case "2":
+//			2.마이페이지 화면
+			JOptionPane.showMessageDialog(null, service.getMyPage(member));
+			break;
+		case "3":
+//			3.비번변경 화면
+			temp = JOptionPane.showInputDialog("로그인 정보와 새로 바꿀 비번을 입력하세요");
+			member = new MemberBean();
+			arr = temp.split(",");
+			member.setId(arr[0]);
+			member.setPw(arr[1]+","+arr[2]);
+			JOptionPane.showMessageDialog(null, service.changePassword(member));
+			break;
+			
+		case "4":
+//			4.회원탈퇴
+			
+			
+			
+		case "5":
+//			5.아이디 존재 체크
+			String id = "";
+			JOptionPane.showInputDialog("아이디 입력:");
+			member.setId(id);
+			JOptionPane.showMessageDialog(null, service.existId(id));
+			break;
+			
+		case "6":
+//			6.로그인
+			temp = JOptionPane.showInputDialog("아이디,비밀번호입력");
+			member = new MemberBean();
+			arr = temp.split(",");
+			member.setId(arr[0]);
+			member.setPw(arr[1]);
+			JOptionPane.showMessageDialog(null, service.login(member));
+			break;
+			
+		case "7":
+//			7. 회원목록
+			JOptionPane.showMessageDialog(null, service.list());
+			break;
+			
+		case "8":
+//			8.아이디 검색 
+			String searchId = JOptionPane.showInputDialog("검색 ID");
+			member = service.findById(searchId);
+			JOptionPane.showMessageDialog(null, member);
+			break;
+			
+		case "9":
+//			9.이름 검색
+//			String serchName = JOptionPane.showInputDialog("검색 이름");
+//			member = service.findName(serchName);
+//			JOptionPane.showMessageDialog(null, member);
+			MemberBean[] memberarr = service.findName(JOptionPane.showInputDialog("이름검색:"));
+			JOptionPane.showMessageDialog(null, memberarr);
+		case "10":
+//			10.전체 회원수 
+			JOptionPane.showMessageDialog(null, "총 회원수 ");
+			JOptionPane.showMessageDialog(null, service.counAll());
+			
+			
+		default:
+		
+			break;
+			
+			
+			
+			
+		}
+	}
+	}
 }
+	
+	
+	
+	
+	
+	
+	
+
+	
+
--- service/MemberService.java
+++ service/MemberService.java
@@ -1,5 +1,170 @@
 package com.bitcamp.service;
-
+import com.bitcamp.domain.MemberBean;
 public class MemberService {
-
+	private MemberBean[] members ;
+	private int count;
+	
+	
+//기본 생성자 
+	public MemberService() {
+		this.members = new MemberBean[10];
+		this.count = 0;
+	}
+	
+	
+	/*************************************
+	 * 사용자 기능 
+	 *//**********************************/
+		
+	/**
+	 * 1.회원가입 
+	*/
+		public String join(MemberBean param) {
+			String msg ="회원가입 되셨습니다";
+			members[count] = param; 
+			count++;
+			return msg;
+		}
+	//*****************************************
+		/**
+		 * 2.Mypage 
+		*/	
+		public String getMyPage(MemberBean param) {
+			return param.toString();
+			}
+		
+		/**
+		 * 3.비번수정
+		 *비번수정 아이디 옛날 비번 신규비번 입력받아서 옛날비번 일치하면 신규비번으로 변경
+		 *비번 변경후 로그인을 실행해서 새로 바뀐 비번이 로그인성공 ,옛날비번 로그인 실패
+		 */
+		
+		public String changePassword(MemberBean param) {
+			String msg = "비밀번호가 변경되었습니다";
+			String id = param.getId();
+			String pw = param.getPw();
+			String[] pws = pw.split(",");
+			String oldPw = pws[0];
+			String newPw = pws[1];
+			
+			for(int i=0;i<count;i++) {
+				if(param.getId().equals(members[i].getId())
+						&&param.getPw().equals(members[i].getPw())) {
+						 members[i].setPw(pws[1]);
+						 break;
+						 }
+			}
+			return msg;
+		}
+		/**
+		 * 4.회원탈퇴
+		 */
+		public String withDraw(MemberBean param) {
+			String msg = "회원탈퇴 완료";
+			
+			return msg;
+					
+		}
+		
+		
+		/**
+		 * 5.아이디 존재 체크
+		 */
+		public String existId(String id) {
+			String msg = "가입가능한 아이디입니다.";
+			for(int i=0;i<count;i++) {
+				if(id.equals(members[i].getId())) {
+					msg = "이미 존재한 아이디입니다.";
+					break;
+				}
+			}
+			return msg;
+		}
+		
+		
+		/**
+		 * 6.로그인
+		 * 파라미터로 아이디 패스워드만 입력받은 상태
+		 */
+		public String login(MemberBean param) {
+			String msg = "로그인 실패";
+			for(int i=0;i<count;i++) {
+				if(param.getId().equals(members[i].getId())
+						&& param.getPw().equals(members[i].getPw())) {
+					msg = "로그인 성공";
+					break;
+				}
+			}
+			
+			return msg;
+		}
+				
+	/***********************************************************
+	 * <관리자 기능>
+	 * 1.회원 목록
+	 * 2.아이디 검색
+	 * 3.이름 검색
+	 * 4.전체 회원 수 
+	 *************************************************************/
+	
+		/**
+		 * 1.회원목록
+		*/
+		 public String list() {
+			 String msg = "";
+			 for(int i=0;i<count;i++) {
+//			Q 이 부분에서 카운트를 미만이아니라 <=이렇게 연산자를 써야되는거 아닌가여?		 
+				 msg += members[i].toString()+", \n";
+			 }
+			 return msg;
+		 }
+		 /**
+		  * 2.아이디 검색
+		  */
+		 public MemberBean findById(String id) {
+			 MemberBean member = new MemberBean();
+			 for(int i=0;i<count;i++) {
+				 if(id.equals(members[i].getId())) {
+					 member = members[i];
+				 }
+			 }
+			 
+			 return member;
+		 }
+		 /**
+		  * 3.이름 검색
+		  */
+		 
+		 public MemberBean[] findName(String name) {
+			 int j = 0;//동명이인 카운트 세는 변수
+			 for(int i=0;i<count;i++) {
+				 if(name.equals(this.members[i].getName())) {
+					 j++;
+				 }
+			 }
+			 MemberBean[] members = new MemberBean[j];
+			 j=0;
+			 for(int i=0;i<count;i++) {
+				 if(name.equals(this.members[i].getName())) {
+					 members[j] = this.members[i];
+					 j++;
+					 if(members.length == j) {
+						 break;
+					 }
+					 
+				 }
+				 members[i] = this.members[i];
+			 }
+			 return members;
+		 }
+		 
+		 /**
+		  * 4.총 회원수
+		  */
+		 public String counAll() {
+			 return "총회원수 : "+count;
+		 }
 }
+
+
+
